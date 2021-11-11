605b76aebb7ea5794bd2b9e79c7619b9924338cb
Zach Rachedi <zach@cyboticx.com>
2021-11-11 22:36:26 +0100

    router creation

--- src/App.ts
+++ src/App.ts
@@ -2,18 +2,26 @@ import * as http from 'http';
 import cors from 'cors';
 import express, { Express } from 'express';
 import { Api404Error } from './app/lib/ErrorHandler';
-import { ErrorHandlerMiddleware } from './app/http/Middlewares/ErrorHandlerMiddleware';
+import { ErrorHandlerMiddleware } from './app/http/Middlewares';
+import { PartyPlanController } from './app/http/Controllers';
+import { celebrate, Segments, errors as celebrateMiddleware } from 'celebrate';
+import PartyPlanPayload from './app/http/Payloads/PartyPlanPayload';
 
 export default class App {
     constructor(private readonly app: Express = express()) {
-        //Simplistic health check
+        // Simplistic health check
         this.app.get('/', (req, res) => res.send('ok'));
 
         // Enable Cross Origin Resource Sharing to all origins by default
         app.use(cors());
 
-        //Only parse json requests
+        // Only parse json requests
         this.app.use(express.json());
+
+        // API routes
+        app.get('/party_plan', celebrate({ [Segments.QUERY]: PartyPlanPayload.get }), PartyPlanController.get);
+
+        this.app.use(celebrateMiddleware());
 
         // Catch 404 and forward to error handler
         app.use((req, res, next) => next(new Api404Error()));
